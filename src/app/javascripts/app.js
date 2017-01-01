94cddbddb66a4e5d620802c1e9cb470eb21b5c79
Laurence Kirk <laurence@extropy.io>
2017-01-01 11:34:55 +0000

    javascript example

--- src/app/javascripts/app.js
+++ src/app/javascripts/app.js
@@ -1,8 +1,14 @@
-var accounts;
-var account;
+  var defs = require('./defs.json');
+  var Web3 = require('web3');
+    if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
 
+    var accounts;
+    var account;
 
-window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
     if (err != null) {
       alert("There was an error fetching your accounts.");
@@ -16,7 +22,56 @@ window.onload = function() {
 
     accounts = accs;
     account = accounts[0];
-
-
   });
+
+
+   // now set up contract definitions
+   var uDataAbi = defs.userDataAbi;
+   var uDataAddress = defs.userDataAddress;
+   var uC = web3.eth.contract(uDataAbi);
+   var userDataContract = uC.at(uDataAddress);
+
+   console.log("contract at " + uDataAddress);
+  //  var data = userDataContract.owner.call();
+  //  console.log((data));
+   setUpFilter(userDataContract);
+   getOwner(userDataContract);
+   addUser(userDataContract);
+
+   //getUserScore(userDataContract,"0x6da8a7f1484de907c4198ba6ed6880e88b834e2f")
+
+  function setUpFilter(contract){
+      var filter = web3.eth.filter([contract.userAdded]);
+      filter.watch(dataEvent);
+
+  }
+
+  function dataEvent (error,result){
+   if(!error) {
+      console.log("User Added");
+       var userAccount = '0x' + result.data.slice(26,66);
+       console.log("New User is " + userAccount );
+
+    } else {
+      console.error(new Date() + " " + error);
+
+  }
+}
+
+function getOwner(contract){
+  var data = contract.owner.call();
+  console.log("Owner is " + (data.toString()));
+}
+
+  function getUserScore(contract,userAddress){
+     var data = contract.getUserData.call(userAddress);
+     console.log("Score is " + (data[1].toString()));
+
+  }
+
+
+function addUser(contract){
+
+  var Tx = contract.addUserData.sendTransaction("0x6da8a7f1484de907c4198ba6ed6880e88b834e2f", "123456", "89", "abcdefg12345", "laurence", "Turl St, Oxford",{from: web3.eth.accounts[0],gas: 2000000});
+
 }
