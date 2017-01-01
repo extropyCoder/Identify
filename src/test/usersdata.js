67d8352c3d8dd6bd3e6563ddfe91aff77808bd31
Laurence Kirk <laurence@extropy.io>
2017-01-01 07:35:32 +0000

    new test

--- src/test/usersdata.js
+++ src/test/usersdata.js
@@ -29,12 +29,23 @@ contract('UserData', function(accounts) {
             assert.equal(num.valueOf(), 0, "user count incorrect");
             usersdata.addUserData.sendTransaction("0x0f5adee83cfd79ea1dc8e771212dc77329f8d79c", "123456", "89", "abcdefg12345", "laurence", "Turl St, Oxford");
             return usersdata.getRegisteredCount.call().then(function(num) {
-                console.log("number of users is now " + num);
-                return assert.equal(num.valueOf(), 1, "user count incorrect");
+              return assert.equal(num.valueOf(), 1, "user count incorrect");
             });
         });
     });
 
+    it("adds a user and gets details", function() {
+        usersdata = UserData.deployed();
+        return usersdata.getRegisteredCount.call().then(function(num) {
+            console.log("number of active members is " + num);
+            assert.equal(num.valueOf(), 1, "user count incorrect");
+            usersdata.addUserData.sendTransaction("0x0aa276db6a32837d7a4663a31665ef111091866a", "123456", "92", "nihsodu876875", "john", "Broad St, Oxford");
+            return usersdata.getUserData.call("0x0aa276db6a32837d7a4663a31665ef111091866a").then(function(newdata) {
+                //console.log("user data is  " + newdata);
+                return assert.equal(newdata[1], 92, "user score incorrect");
 
+            });
+        });
+    });
 
 });
